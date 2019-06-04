8b2c4fba24f60f45ebdc0598fac726a8f72f5f60
Nitish <nitish@maropost.com>
2019-06-04 18:08:05 +0530

    Side menu changes

--- Timely/ViewControllers/SideMenuVC.swift
+++ Timely/ViewControllers/SideMenuVC.swift
@@ -13,7 +13,13 @@ class SideMenuVC: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "reuseIdentifier")
-        self.tableView.contentInset = UIEdgeInsets(top: 30, left: 20, bottom: 0, right: 0)
+        self.tableView.contentInset = UIEdgeInsets(top: 30, left: 5, bottom: 0, right: 0)
+        
+        let labelFooter = UILabel(frame: CGRect(x: 20, y: 10, width: 300, height: 20))
+        labelFooter.text = "Version \(Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") ?? "")"
+        labelFooter.textColor = UIColor.darkGray
+       // labelFooter.textAlignment = .right
+        self.tableView.tableFooterView = labelFooter
     }
 
     // MARK: - Table view data source
@@ -45,7 +51,7 @@ class SideMenuVC: UITableViewController {
         cell.selectionStyle = .none
         switch indexPath.row {
         case 0:
-            let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+            let imageView = UIImageView(frame: CGRect(x: cell.frame.size.width/2 - 105, y: 0, width: 200, height: 200))
             imageView.image = UIImage(named: "test_user.png")
             imageView.layer.cornerRadius = (imageView.frame.size.width)/2
             imageView.layer.borderColor = UIColor.lightGray.cgColor
@@ -56,25 +62,32 @@ class SideMenuVC: UITableViewController {
             let labeltitle = UILabel(frame: CGRect(x: 0, y: 210, width: self.view.frame.size.width, height: 25))
             labeltitle.text = "John Gloom"
             labeltitle.textColor = TimelyColors.shared.kColorRedTheme
+            labeltitle.textAlignment = .center
             labeltitle.font = UIFont(name: "Lato", size: 24)
             cell.contentView.addSubview(labeltitle)
             
             let labelEmail = UILabel(frame: CGRect(x: 0, y: 240, width: self.view.frame.size.width, height: 25))
             labelEmail.text = "jgloom@gmail.com"
-            labelEmail.textColor = TimelyColors.shared.kColorRedTheme
+            labelEmail.textColor = UIColor.blue
+            labelEmail.textAlignment = .center
             labelEmail.font = UIFont(name: "Lato", size: 20)
             cell.contentView.addSubview(labelEmail)
             
         case 1:
             cell.textLabel?.text = "Calendar"
+            cell.imageView?.image = UIImage(named: "calendar.png")
         case 2:
             cell.textLabel?.text = "Terms"
+            cell.imageView?.image = UIImage(named: "terms.png")
         case 3:
             cell.textLabel?.text = "Check-in"
+            cell.imageView?.image = UIImage(named: "check_in.png")
         case 4:
             cell.textLabel?.text = "Leave"
+            cell.imageView?.image = UIImage(named: "leave.png")
         case 5:
             cell.textLabel?.text = "About Us"
+            cell.imageView?.image = UIImage(named: "about_us.png")
             
         default:
             print("")
@@ -86,14 +99,6 @@ class SideMenuVC: UITableViewController {
         
         return cell
     }
-    
-//    override func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
-//        let labelFooter = UILabel(frame: CGRect(x: 10, y: 10, width: self.view.frame.size.height, height: 20))
-//        labelFooter.text = "\(Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") ?? "")"
-//        labelFooter.textColor = UIColor.lightGray
-//        return labelFooter
-//    }
-//
 
     /*
     // Override to support conditional editing of the table view.
